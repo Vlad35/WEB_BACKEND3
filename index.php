d517d53625378f6c1f28994a0ffdf63c251772bc
Vlad35 <90512038+Vlad35@users.noreply.github.com>
2022-04-25 18:12:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 if ($_SERVER['REQUEST_METHOD'] != 'POST'){
-	print_r('Does not accept non POST methods');
+	print_r('ONLY POST METHODS!!');
 }
 $errors = FALSE;
-if(empty($_POST['field-name-1']) || !isset($_POST['field-name-4']) || empty($_POST['field-email']) || empty($_POST['field-date']) || empty($_POST['bio-field']) || empty($_POST['checkbox']) || $_POST['checkbox'] == false){
+
+if(empty($_POST['name-1']) || !isset($_POST['name-4']) || empty($_POST['email']) || empty($_POST['date']) || empty($_POST['bio']) || empty($_POST['checkbox']) || $_POST['checkbox'] == false){
 	print_r('Empty fields!');
 	exit();
 }
-if(!is_numeric($_POST['radio-group-2'])){
-	print_r('Limb field is non-numeric');
+if(!is_numeric($_POST['radio-2'])){
+	print_r('Limb field is not a number');
 }
-print_r("Non null data... <br/>");
-$name = $_POST['field-name-1'];
-$email = $_POST['field-email'];
-$birth = $_POST['field-date'];
-$bio= $_POST['bio-field'];
-$bioregex = "/^\s*\w+[\w\s\.,-]*$/";
-$regex = "/^\w+[\w\s-]*$/";
-$dateregex = "/^\d{4}-\d{2}-\d{2}$/";
-$mailregex = "/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/";
-if(!preg_match($regex,$name)){
+$name = $_POST['name-1'];
+$email = $_POST['email'];
+$birth = $_POST['date'];
+$bio= $_POST['bio'];
+$limbs = $_POST['radio-2'];
+$sex = $_POST['radio-1'];
+$BioReg = "/^\s*\w+[\w\s\.,-]*$/";
+$NameReg = "/^\w+[\w\s-]*$/";
+$DateReg = "/^\d{4}-\d{2}-\d{2}$/";
+$MailReg = "/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/";
+if(!preg_match($NameReg,$name)){
 	print_r('Invalid name format');
 	exit();
 }
@@ -28,7 +30,7 @@ if($limbs < 1 || $limbs > 5){
 	print_r('Invalid am of limbs');
 	exit();
 }
-if(!preg_match($dateregex,$birth)){
+if(!preg_match($DateReg,$birth)){
 	print_r('Invalid birth format');
 	exit();
 }
@@ -37,30 +39,35 @@ if (!checkdate($matches[0][1],$matches[0][2],$matches[0][0])){
 	print_r('Date does not exist');
 	exit();
 }
-if(!preg_match($bioregex,$bio)){
+if(!preg_match($BioReg,$bio)){
 	print_r('Invalid bio format');
 	exit();
 }
-if(!preg_match($mailregex,$email)){
+if(!preg_match($MailReg,$email)){
 	print_r('Invalid email format');
 	exit();
 }
 if($sex !== 'male' && $sex !== 'female'){
-	print_r('Invalid sex format');
+	print_r('Then who you are...');
 	exit();
 }
-foreach($superpowers as $checking){
-	if(array_search($checking,$super_list)=== false){
-			print_r('Invalid superpower value!');
-			exit();
-	}
-}
+// foreach($superpowers as $checking){
+// 	if(array_search($checking,$super_list)=== false){
+// 			print_r('Invalid superpower value!');
+// 			exit();
+// 	}
+// }
 $user = 'u47684';
 $pass = '8848410';
-$db = new PDO('mysql:host=212.192.134.20;dbname=u47684', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+//ssh -fNg -L 3307:127.0.0.1:3306 u47684@212.192.134.20
+//lsof -i:3307
+$db = new PDO('mysql:host=127.0.0.1;port=3307;dbname=u47684', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+// $db = new PDO('mysql:host=212.192.134.20;dbname=u47684', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
 try {
-  $stmt = $db->prepare("INSERT INTO dba SET Name=:name, Mail=:email, Birth=:birthdate, Bio=:bio");
-  $stmt->bindParam(':name', $name);
+  $stmt = $db->prepare("INSERT INTO DataTable SET name=:name, email=:email, birthdate=:birthdate, bio=:bio");
+  $stmt->bindParam('name', $name);
   $stmt->bindParam(':email', $email);
   $stmt->bindParam(':birthdate', $birth);
   $stmt->bindParam(':bio', $bio);
@@ -69,10 +76,11 @@ try {
   print_r($stmt->errorInfo());
   exit();
   }
+}
 catch(PDOException $e){
   print('Error : ' . $e->getMessage());
   exit();
 }
 
-print_r("Succesfully added new stuff, probably...");
+print_r("Added!");
 ?>
